26b41a934eba28fde8063e8021e502f051a37363
AnubhavRathore <86161584+AnubhavRathore@users.noreply.github.com>
2021-10-02 17:06:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,100 @@
+from tkinter import *
+import math
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 0
+timer = None
+# ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    timer_label.config(text="Timer", fg=GREEN)
+    canvas.itemconfig(timer_text, text="00:00")
+    check_label.config(text="")
+    global reps
+    reps = 0
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        timer_label.config(text="Break", fg=RED)
+        countdown(long_break_sec)
+    elif reps % 2 == 0:
+        timer_label.config(text="Break", fg=PINK)
+        countdown(short_break_sec)
+    else:
+        timer_label.config(text="Work", fg=GREEN)
+        countdown(work_sec)
+
+    """My own method"""
+    # if reps == 1 or reps == 3 or reps == 5 or reps == 7:
+    #     print("b")
+    #     countdown(6)
+    #     # print("c")
+    # elif reps == 2 or reps == 4 or reps == 6:
+    #     print("d")
+    #     countdown(3)
+    #     print("e")
+    # elif reps == 8:
+    #     print("f")
+    #     countdown(4)
+    #     print("g")
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def countdown(remaining_time):
+    # print(remaining_time)
+    remaining_time_min = math.floor(remaining_time / 60)
+    """Below method is also correct."""
+    # remaining_time_min = remaining_time // 60
+    remaining_time_sec = remaining_time % 60
+    if remaining_time_sec < 10:
+        remaining_time_sec = f"0{remaining_time_sec}"
+    canvas.itemconfig(timer_text, text=f"{remaining_time_min}:{remaining_time_sec}")
+    if remaining_time > 0:
+        global timer
+        timer = window.after(1000, countdown, remaining_time - 1)
+    else:
+        mark = ""
+        start_timer()
+        if reps % 2 == 0:
+            for _ in range(int(reps/2)):
+                mark += "✔"
+            check_label.config(text=mark)
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 25, "bold"), fill="white")
+canvas.grid(row=1, column=1)
+
+# countdown(5)
+
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+timer_label.grid(row=0, column=1)
+
+check_label = Label(fg=GREEN, bg=YELLOW)
+check_label.grid(row=3, column=1)
+
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
+start_button.grid(row=2, column=0)
+
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
+reset_button.grid(row=2, column=2)
+
+window.mainloop()
+
